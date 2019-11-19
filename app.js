9e7c0579a25668117821164f8a881b60d64ade5e
Anand Altankhuyag <effand9668@gmail.com>
2019-11-19 17:18:36 +0800

    added mongoose

--- app.js
+++ app.js
@@ -5,34 +5,60 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json());
 
 app.use(
   '/graphql',
   graphQlHttp({
     schema: buildSchema(`
+      type Event {
+        _id: ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+
+      }
+
+      input EventInput {
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+      }
 
       type RootQuery {
-        events: [String!]!
+        events: [Event!]!
       }
 
       type RootMutation {
-        createEvent(name: String): String
+        createEvent(eventInput: EventInput): Event
       }
 
-      schema{
+      schema { 
         query: RootQuery
         mutation: RootMutation
       }
     `),
+
     rootValue: {
       events: () => {
-        return ['Romantic Cook', 'Sailing', 'All-night coding'];
+        return events;
       },
       createEvent: args => {
-        const eventName = args.name;
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: args.eventInput.date
+        };
 
-        return eventName;
+        events.push(event);
+
+        return event;
       }
     },
     graphiql: true
